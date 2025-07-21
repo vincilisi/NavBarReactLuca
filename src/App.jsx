9c44ad60036e6ec513697781ccd5e2b764c0d751
vincilisi <lisitano95@gmail.com>
2025-07-21 20:27:26 +0200

    correzione posizione cartella components e creazione di cambio arcobaleno

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import Navbar from './assets/components/navbar'
+import Navbar from './components/navbar'
 
 function App() {
   let title = 'Prova NavFiga'
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,27 @@
-import React, { useState } from "react";
+import React, { use, useEffect, useState } from "react";
 import './navbar.css';
 
 const Navbar = () => {
-   const [isOpen, setIsOpen] = useState(false);
-  const toggleDropdown = () => setIsOpen(!isOpen);
+  // State to manage dropdown visibility
+    const [isOpen, setIsOpen] = useState(false);
+    const toggleDropdown = () => setIsOpen(isOpen => !isOpen);
+    // color arcobaleno
+    useEffect(() => {
+  const arcobaleno = setInterval(() => {
+    const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+    // Applica il colore di sfondo alla navbar
+    const navbarElement = document.querySelector('.navbar');
+    if (navbarElement) {
+      navbarElement.style.backgroundColor = randomColor;
+    }
+  }, 500); // Cambia colore ogni 500 ms
+  // Pulisci l'intervallo quando il componente viene smontato
+
+  return () => clearInterval(arcobaleno);
+}, []);
+
+
   return (
     <nav className="navbar">
         <div className="logo"><img className="imglogo" src="https://imgs.search.brave.com/89qXELwgP6g3psNsHvwDOEdq3LbHyg_JA94nvl88j7w/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly9pbWFn/ZXMuc2Vla2xvZ28u/Y29tL2xvZ28tcG5n/LzE2LzIvbWNkb25h/bGRzLWxvZ28tcG5n/X3NlZWtsb2dvLTE2/ODQ2NC5wbmc" alt="logo" /></div>
